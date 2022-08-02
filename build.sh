011e5a576bc8986a61e9f24f67b418252b003e2f
clhex <100755123+clhexftw@users.noreply.github.com>
2022-08-02 19:13:12 +0530

    Create build.sh

---
+++ build.sh
@@ -0,0 +1,13 @@
+# sync rom
+repo init --depth=1 --no-repo-verify -u https://github.com/P-404/android_manifest -b shinka -g default,-mips,-darwin,-notdefault
+git clone https://github.com/p404-vayu/local_manifest --depth=1 -b main .repo/local_manifests
+repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
+
+# build rom
+source build/envsetup.sh
+lunch p404_vayu-userdebug
+export SELINUX_IGNORE_NEVERALLOWS=true
+#export ALLOW_MISSING_DEPENDENCIES=true
+export WITH_GAPPS=true
+export TZ=Asia/Kolkata #put before last build command
+make bacon
